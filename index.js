3a7c5fe749d27e4f7096a51a558ff8005d5a0855
Daniel Figueroa <dfigueroa7007@gmail.com>
2020-11-16 13:50:05 -0800

    Added question structure and first 2 questions.

--- index.js
+++ index.js
@@ -3,7 +3,60 @@ const fs = require('fs');
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    // Questions 1: Title
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title for your README file?"
+      },
+      // Questions 2: Short description explaining: - What the app is for, - how to use it, - how to install it, - how to report issues,
+      {
+        type: "input",
+        name: "description",
+        message: "Write a short description explaining: - What the app is for, - how to use it, - how to install it, - how to report issues"
+      },
+    // Questions 3: Table of Contents
+      {
+        type:
+        name:
+        message:
+      },
+      // Questions 4: Installation
+      {
+        type:
+        name:
+        message:
+      },
+      // Questions 5: Usage
+      {
+        type:
+        name:
+        message:
+      },
+      // Questions 6: License
+      {
+        type:
+        name:
+        message:
+      },
+      // Questions 7: Contributing
+      {
+        type:
+        name:
+        message:
+      },
+      // Questions 8: Tests
+      {
+        type:
+        name:
+        message:
+      },
+      // Questions 9: Questions
+      {
+        type:
+        name:
+        message:
+      }
   ])
   .then(answers => {
     // Use user feedback for... whatever!!
