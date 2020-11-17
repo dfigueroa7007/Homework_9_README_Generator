c3842eb2b2ce3572fcb453c800d5ef7136ce3c4e
Daniel Figueroa <dfigueroa7007@gmail.com>
2020-11-16 18:05:45 -0800

    Modified questions structure, added write functions, and added information to generateMarkdown.js in utils.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+import generateMarkdown from './utils/generateMarkdown.js';
 
 inquirer
   .prompt([
@@ -9,78 +10,78 @@ inquirer
         name: "title",
         message: "What is the title for your README file?"
       },
-      // Questions 2: Short description explaining: - What the app is for, - how to use it, - how to install it, - how to report issues,
+      // Questions 2: Short description explaining of the app.
       {
         type: "input",
         name: "description",
-        message: "Write a short description explaining: - What the app is for, - how to use it, - how to install it, - how to report issues"
-      },
-    // Questions 3: Table of Contents
+        message: "Describe what the app does and what it is for here:"
+      }
+      // Questions 3: Installation
       {
-        type:
-        name:
+        type: "input",
+        name: "installation"
+        message: "How is the app installed?"
+      },
+      // Questions 4: Usage
+      {
+        type: "input",
+        name: "usage"
+        message: "What is the intended usage of this app?"
+      },
+      // Questions 5: License
+      {
+        type: "list"
+        name: "license"
         message:
       },
-      // Questions 4: Installation
+      // Questions 6: Contributing
       {
-        type:
-        name:
+        type: "input",
+        name: "contributing"
         message:
       },
-      // Questions 5: Usage
+      // Questions 7: Tests
       {
-        type:
-        name:
+        type: "input",
+        name: "tests"
         message:
       },
-      // Questions 6: License
+      // Questions 8: Questions
       {
-        type:
-        name:
-        message:
-      },
-      // Questions 7: Contributing
-      {
-        type:
-        name:
-        message:
-      },
-      // Questions 8: Tests
-      {
-        type:
-        name:
-        message:
-      },
-      // Questions 9: Questions
-      {
-        type:
-        name:
+        type: "input",
+        name: "questions"
         message:
       }
   ])
   .then(answers => {
-    // Use user feedback for... whatever!!
+    console.log(answers)
+    writeToFile(answers)
   })
   .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
+    // if(error.isTtyError) {
+    //   // Prompt couldn't be rendered in the current environment
+    // } else {
+    //   // Something else when wrong
+    // }
+    console.log(error)
   });
 
 // array of questions for user
-const questions = [
+// const questions = [
 
-];
+// ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile() {
+  fs.writeFile("README.md", function(err){console.log(err || "File written successfully!")})
 }
 
 // function to initialize program
 function init() {
-
+  prompt(questions).then(answers => {
+      const response = generateMarkdown(answers);
+      writetoFile("./README.md", response);
+  })
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
 
+  ## Table of Contents
+  *[Description](#description)
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#licesnse)
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+
+  ##Description
+  ${data.description}
+
+  ##Installation
+  ${data.description}
+
+  ##Usage
+  ${data.description}
+
+  ##License
+  ${data.description}
+
+  ##Contributing
+  ${data.description}
+
+  ##Tests
+  ${data.description}
+
+  ##Questions
+  ${data.description}
 `;
 }
 
