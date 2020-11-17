2944269214f64e6c31e0129d71ea240ab8738c14
Daniel Figueroa <dfigueroa7007@gmail.com>
2020-11-16 18:36:37 -0800

    Fixed path file in utils

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 inquirer
   .prompt([
